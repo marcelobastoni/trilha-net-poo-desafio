f447d4466c3c1897ac0650374f66476ef640790d
marcelobastoni <marcelobastoni@gmail.com>
2024-11-24 03:27:04 -0300

    Alterando as Classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("99999-9999","12","11111",128);
+
+Console.WriteLine("Testando Nokia:");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp ");
+
+Iphone iphone = new Iphone("88888-8888","16","99999",256);
+
+Console.WriteLine("Testando Iphone:");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp ");
